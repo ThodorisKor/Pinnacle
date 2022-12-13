ccabe6d47345ce015f4d73ac9106780cd84cdab6
billdram <dravasilis@gmail.com>
2022-12-13 17:19:42 +0200

    sada

--- www/pinnacle.php
+++ www/pinnacle.php
@@ -14,9 +14,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="script.js"></script>
     <title>Pinnacle</title>
- <script>
-    $("#head").prepend("blablabla");
- </script>
+ 
 </head>
 <body>
  <?php 
