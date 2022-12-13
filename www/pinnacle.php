1c43dc44781358bb94d3b2e1c7eea6480ce42cdc
billdram <dravasilis@gmail.com>
2022-12-13 18:47:46 +0200

    create requests.php,game_status.php

--- www/pinnacle.php
+++ www/pinnacle.php
@@ -19,10 +19,10 @@
 <body>
  <?php 
   require_once "../lib/dbConnect.php";
-  require_once "../lib/deck.php";
   require_once "../lib/login.php"; 
-  
- ?>
+  require_once "../lib/game_status.php"; 
+?>
+ 
     <div class="container text-center pt-5" >
         <div class="row justify-content-center pt-5">
             <div class="col-md-5">
@@ -41,47 +41,11 @@
     </div>
 
    
-    
+ 
+  
+  
 
-    <?php
-    
-    /*   
-     $method = $_SERVER['REQUEST_METHOD'];
-    $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-     
-    $input = json_decode(file_get_contents('php://input'),true);
-
-   
-    switch ($r=array_shift($request)) {
-        case 'deck' : 
-        switch ($b=array_shift($request)) {
-            case '':
-            case null: handle_deck($method);
-            break;
-            case 'piece': // handle_piece($method, $request[0],$request[1],$input);
-                        break;
-            case 'player': // handle_player($method, $request[0],$input);
-                        break;
-            default: header("HTTP/1.1 404 Not Found");
-                    break;
-        }
-        break;
-        default: 	
-        header("HTTP/1.1 404 Not Found");
-        exit;
-    }
-    function handle_deck($method){
-        if($method=='GET'){
-            show_deck();
-        }
-        else if($method=='POST'){
-            reset_deck();
-        }
-        else{
-            header('HTTP/1.1405 Method Not Allowed');
-        }
-    }
-    */
-    ?> 
+ 
 </body>
 </html>
+
