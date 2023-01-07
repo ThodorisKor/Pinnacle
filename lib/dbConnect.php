45704caea4436b1c98831db43fa80b9cf8f92247
billdram <dravasilis@gmail.com>
2023-01-07 17:19:06 +0200

    Update dbConnect.php

--- lib/dbConnect.php
+++ lib/dbConnect.php
@@ -9,8 +9,10 @@ $pass=$DB_PASS;
 
 
 if(gethostname()=='users.iee.ihu.gr') {
+        echo '<script>alert("mphke sto users")</script>';
 	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2018/it185430/mysql/run/mysql.sock');
 } else {
+        echo '<script>alert("mphke sto else")</script>';
         $mysqli = new mysqli($host, $user, $pass, $db);
         
          
